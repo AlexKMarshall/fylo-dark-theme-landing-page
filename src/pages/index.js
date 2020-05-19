956278a2b819a52ab9265fd16e7a1fb743bc8476
Alex Marshall <alex.k.marshall83@gmail.com>
2020-05-19 16:19:20 +0100

    Add html for key features section

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,37 @@ const IndexPage = () => (
     </p>
     <button>Get Started</button>
 
+    <ul>
+      <li>
+        <header>Access your files, anywhere</header>
+        <section>
+          The ability to use a smartphone, tablet, or computer to access your
+          account means your files follow you everywhere.
+        </section>
+      </li>
+      <li>
+        <header>Security you can trust</header>
+        <section>
+          2-factor authentication and user-controlled encryption are just a
+          couple of the security features we allow to help secure your files.
+        </section>
+      </li>
+      <li>
+        <header>Real-time collaboration</header>
+        <section>
+          Securely share files and folders with friends, family and colleagues
+          for live collaboration. No email attachments required.
+        </section>
+      </li>
+      <li>
+        <header>Store any type of file</header>
+        <section>
+          Whether you're sharing holidays photos or work documents, Fylo has you
+          covered allowing for all file types to be securely stored and shared.
+        </section>
+      </li>
+    </ul>
+
     {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div> */}
