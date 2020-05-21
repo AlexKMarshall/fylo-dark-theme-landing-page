3dc1827cf9dbd05994aad5b70540b146a5367745
Alex Marshall <alex.k.marshall83@gmail.com>
2020-05-21 08:10:57 +0100

    Add image to info section

--- src/components/info-section.js
+++ src/components/info-section.js
@@ -1,19 +1,47 @@
 import React from "react";
 import { Link } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+
+const Illustration = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(
+        relativePath: { eq: "illustration-stay-productive.png" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 720) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <Img
+      fluid={data.placeholderImage.childImageSharp.fluid}
+      alt="Illustration of three people working together"
+    />
+  );
+};
 
 const InfoSection = () => (
-  <section>
-    <h2>Stay productive, wherever you are</h2>
-    <p>
-      Never let location be an issue when accessing your files. Fylo has you
-      covered for all of your file storage needs.
-    </p>
-    <p>
-      Securely share files and folders with friends, family and colleagues for
-      live collaboration. No email attachments required.
-    </p>
-    <Link to={`/`}>See how Fylo works</Link>
-  </section>
+  <div>
+    <Illustration />
+    <section>
+      <h2>Stay productive, wherever you are</h2>
+      <p>
+        Never let location be an issue when accessing your files. Fylo has you
+        covered for all of your file storage needs.
+      </p>
+      <p>
+        Securely share files and folders with friends, family and colleagues for
+        live collaboration. No email attachments required.
+      </p>
+      <Link to={`/`}>See how Fylo works</Link>
+    </section>
+  </div>
 );
 
 export default InfoSection;
