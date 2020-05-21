2bfdf25d42ee496009dd55efe129675273833c13
Alex Marshall <alex.k.marshall83@gmail.com>
2020-05-21 07:50:01 +0100

    Style key features grid

--- src/components/key-features.js
+++ src/components/key-features.js
@@ -1,5 +1,34 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
+import styled from "styled-components";
+
+const StyledUl = styled.ul`
+  max-width: 900px;
+  margin: 0 auto;
+  margin-bottom: 120px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  column-gap: 150px;
+  row-gap: 85px;
+  li {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    p {
+      &:first-child {
+        flex-basis: 95px;
+      }
+    }
+  }
+  h4 {
+    font-size: 20px;
+    font-weight: 700;
+    letter-spacing: 0.05em;
+    margin-top: 35px;
+    margin-bottom: 18px;
+  }
+`;
 
 const KeyFeatures = () => {
   const data = useStaticQuery(graphql`
@@ -21,14 +50,14 @@ const KeyFeatures = () => {
   `);
 
   return (
-    <ul>
+    <StyledUl>
       {data.allMarkdownRemark.nodes.map((node) => (
         <li key={node.id} dangerouslySetInnerHTML={{ __html: node.html }}>
           {/* <h4>{node.frontmatter.title}</h4>
           <p>{node.rawMarkdownBody}</p> */}
         </li>
       ))}
-    </ul>
+    </StyledUl>
   );
 };
 
