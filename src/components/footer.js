62939abc002910c2ccb8a65ca88298909227042b
Alex Marshall <alex.k.marshall83@gmail.com>
2020-05-21 14:15:05 +0100

    Style address

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,20 +3,38 @@ import styled from "styled-components";
 import Navigation from "./navigation";
 import Logo from "./logo";
 
+import iconLocation from "./../images/icon-location.svg";
+
 const StyledFooter = styled.footer`
   margin-left: 110px;
+`;
 
-  Logo {
-    background: red;
+const StyledAddress = styled.ol`
+  margin-left: 30px;
+  position: relative;
+
+  &::before {
+    content: " ";
+    position: absolute;
+    top: 5px;
+    left: -30px;
+    width: 13px;
+    height: 18px;
+    background-image: url(${iconLocation});
   }
 `;
 
 const Footer = ({ children, links }) => (
   <StyledFooter>
     <Logo style={{ marginBottom: `35px` }} />
+    <StyledAddress>
+      <li>14 Tottenham Court Road </li>
+      <li>London</li>
+      <li>England</li>
+      <li>W1T 1JY</li>
+    </StyledAddress>
     <div>
       <ul>
-        <li>14 Tottenham Court Road London England W1T 1JY</li>
         <li>+1-543-123-4567</li>
         <li>example@fylo.com</li>
       </ul>
